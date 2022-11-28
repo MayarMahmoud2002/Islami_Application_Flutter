5d31a148aa032819f119a6c7b27a611a20425934
Mayar Mahmoud <mahmoudmayar251@gmail.com>
2022-11-28 18:48:05 +0200

    add splash screen images

--- lib/homescreen/home_screen.dart
+++ lib/homescreen/home_screen.dart
@@ -6,11 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text (
-          'islami',
-        ),
-      ),
+      appBar: AppBar(),
     );
   }
 }
